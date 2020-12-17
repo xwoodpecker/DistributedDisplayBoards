5edbd5252573c0d67550036174da79be9f40a224
Lukas Roth <lukas.roth252@gmail.com>
2020-12-17 12:11:38 +0100

    added new component for Dashboard

---
+++ board_frontend/src/components/Dashboard.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="boards">
+        <!--<Board  v-for="board in boards" v-bind:key="board.id" msg="Test"></Board>-->
+        <Board msg="Test"></Board>
+    </div>
+</template>
+
+<script>
+    import Board from "./Board";
+
+    export default {
+        name: 'Dasboard',
+        props: {},
+        components: {
+            Board,
+        },
+        data() {
+            return {
+                user: this.$store.state.user,
+            }
+        },
+        methods: {},
+        computed: {},
+        created() {
+
+        },
+        mounted() {
+
+        }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
