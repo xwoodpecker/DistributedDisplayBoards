9b7454d37dcb6274baa9afd31b2ebc49fbbd8a4d
lu-roth <lukas.roth252@gmail.com>
2020-12-23 14:43:57 +0100

    added dashboard + sidebar

--- board_frontend/src/components/Dashboard.vue
+++ board_frontend/src/components/Dashboard.vue
@@ -1,26 +1,52 @@
 <template>
-    <div class="boards">
-        Hier kommt die Übersicht aller Gruppen hin
-        <!--<Board  v-for="board in boards" v-bind:key="board.id" msg="Test"></Board>-->
-        <Board msg="Test"></Board>
+  <div class="page-container">
+    <md-app md-waterfall md-mode="overlap">
+      <md-app-toolbar class="md-primary md-large">
+        <div class="md-toolbar-row">
+          <md-button class="md-icon-button" @click="menuVisible = !menuVisible">
+            <md-icon>menu</md-icon>
+          </md-button>
 
-        <button @click="logout">Ausloggen</button>
-    </div>
+          <span class="md-title">Dashboard</span>
+        </div>
+      </md-app-toolbar>
+
+      <md-app-drawer :md-active.sync="menuVisible">
+       <Sidebar></Sidebar>
+      </md-app-drawer>
+
+      <md-app-content>
+        <p>Hier kommen die Boards hin</p>
+        <p>Hier kommen die Boards hin</p>
+        <p>Hier kommen die Boards hin</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error quibusdam, non molestias et! Earum magnam, similique, quo recusandae placeat dicta asperiores modi sint ea repudiandae maxime? Quae non explicabo, neque.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error quibusdam, non molestias et! Earum magnam, similique, quo recusandae placeat dicta asperiores modi sint ea repudiandae maxime? Quae non explicabo, neque.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error quibusdam, non molestias et! Earum magnam, similique, quo recusandae placeat dicta asperiores modi sint ea repudiandae maxime? Quae non explicabo, neque.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error quibusdam, non molestias et! Earum magnam, similique, quo recusandae placeat dicta asperiores modi sint ea repudiandae maxime? Quae non explicabo, neque.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error quibusdam, non molestias et! Earum magnam, similique, quo recusandae placeat dicta asperiores modi sint ea repudiandae maxime? Quae non explicabo, neque.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error quibusdam, non molestias et! Earum magnam, similique, quo recusandae placeat dicta asperiores modi sint ea repudiandae maxime? Quae non explicabo, neque.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error quibusdam, non molestias et! Earum magnam, similique, quo recusandae placeat dicta asperiores modi sint ea repudiandae maxime? Quae non explicabo, neque.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error quibusdam, non molestias et! Earum magnam, similique, quo recusandae placeat dicta asperiores modi sint ea repudiandae maxime? Quae non explicabo, neque.</p>
+      </md-app-content>
+    </md-app>
+  </div>
 </template>
 
 <script>
-    import Board from "./Board";
+    //import Board from "./Board";
     import authenticationService from "../authenticationService";
-
+    import Sidebar from "@/components/Layout/Sidebar";
     export default {
         name: 'Dashboard',
         props: {},
         components: {
-            Board,
+          //  Board,
+          Sidebar,
         },
         data() {
             return {
                 user: this.$store.state.user,
+                menuVisible: false
             }
         },
         methods: {
@@ -40,6 +66,18 @@
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style lang="scss" scoped>
+.md-app {
+  height: 100vh;
+  width: 100%;
+  border: 1px solid rgba(#000, .12);
+  .md-app-toolbar {
+      background-color: #34495e;
+  }
+}
 
+ .md-drawer {
+     width: 230px;
+     max-width: calc(100vw - 125px);
+ }
 </style>
