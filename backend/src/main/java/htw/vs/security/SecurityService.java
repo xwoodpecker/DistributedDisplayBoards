3d7334e3785da49f6a38d3ce12cc630ee68d774b
Maverick Studer <maverick.studer95@gmail.com>
2020-12-30 17:52:19 +0100

    security

--- backend/src/main/java/htw/vs/security/SecurityService.java
+++ backend/src/main/java/htw/vs/security/SecurityService.java
@@ -1,10 +1,28 @@
 package htw.vs.security;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
+
+import java.security.Principal;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Component("securityService")
 public class SecurityService {
     public boolean hasPermission(String key) {
+        return true;
+    }
+
+    public boolean hasPermission(Authentication authentication, String foo) {
+        Set<String> roles = authentication.getAuthorities().stream()
+                .map(r -> r.getAuthority()).collect(Collectors.toSet());
+
+        Principal principal = (Principal) authentication.getPrincipal();
+        String name = principal.getName();
+
+        if(roles.contains("ROLE_COORDINATOR"))
+            return true;
+
         return true;
     }
 }
