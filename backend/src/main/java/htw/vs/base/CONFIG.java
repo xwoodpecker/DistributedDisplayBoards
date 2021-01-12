faf30d4f83dc32ee0cef3ef15c67c65b6bec1887
Maverick Studer <maverick.studer95@gmail.com>
2021-01-12 17:49:51 +0100

    default supervisor password changed

--- backend/src/main/java/htw/vs/base/CONFIG.java
+++ backend/src/main/java/htw/vs/base/CONFIG.java
@@ -2,9 +2,9 @@ package htw.vs.base;
 
 public class CONFIG {
     public static final String BASIC_TOPIC = "/boards/";
-    public static final String DEFAULT_ADMIN_PASSWORD = "admin";
+    public static final String DEFAULT_SUPERVISOR_PASSWORD = "supervisor";
     public static final String CENTRAL_BOARD_NAME = "central";
-    public static final String BROKER_HOST = "192.168.178.2";
+    public static final String BROKER_HOST = "v4.deuersprech.de";
     public static final int BROKER_PORT = 61613;
     public static final String BROKER_LOGIN = "guest";
     public static final String BROKER_PASSCODE = "guest";
