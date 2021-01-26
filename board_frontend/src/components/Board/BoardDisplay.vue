b7f2ef01a4b3facfc7043703ef0c950803fac51f
NewYearNewPhil <pallweyer@htwsaar.de>
2021-01-26 20:45:44 +0100

    implemented BoardDisplay logic & progress bar

--- board_frontend/src/components/Board/BoardDisplay.vue
+++ board_frontend/src/components/Board/BoardDisplay.vue
@@ -1,48 +1,104 @@
 <template>
-    <agile ref="carousel" v-bind:options="carouselOptions">
-        <div class="slide">
-            <h3>slide 1</h3>
-        </div>
-        
-        <div class="slide">
-            <h3>slide n</h3>
-        </div>
+  <div>
+    <agile ref="carousel" v-if="messages" v-bind:options="carouselOptions">
+      <div v-for="message in messages" :key="message.id">
+        <BoardDisplayMessage v-bind:message="message" />
+        <!-- todo (low prio) make sure currently not displayed progress-bars are not constantly being updated -->
+        <md-progress-bar
+          md-mode="determinate"
+          :md-value="amount"
+        ></md-progress-bar>
+      </div>
     </agile>
+  </div>
 </template>
 
 <script>
+import gsap from "gsap";
+import BoardDisplayMessage from "@/components/Board/BoardDisplayMessage.vue";
 export default {
   name: "BoardDisplay",
   props: {
-    messages: Array,
+    //messages: Array,
+    autoStart: {
+      type: Boolean,
+      default: true,
+    },
   },
-    data() {
+  components: {
+    BoardDisplayMessage,
+  },
+  data() {
     return {
       carouselOptions: {
         dots: false,
         navButtons: false,
-        speed: 300, //default
-        timing: "ease", //default
-      }
+        rtl: true,
+        speed: 1000,
+      },
+      amount: 0,
+      currentSlide: 0,
+      animation: undefined,
+      messages: [
+        {
+          id: 1,
+          content: "This is a message!",
+          duration: 5,
+        },
+        {
+          id: 2,
+          content: "This is a different message!",
+          duration: 5,
+        },
+      ],
     };
   },
   methods: {
-    play(){
-
+    start() {
+      this.startAnimation(
+        this.messages[this.$refs.carousel.currentSlide].duration
+      );
     },
-    pause(){
-
+    play() {
+      if (this.animation) this.animation.play();
+      else this.start();
     },
-    goto(){
-
-    }
+    pause() {
+      if (this.animation) this.animation.pause();
+    },
+    goto() {},
+    next() {
+      this.$refs.carousel.goToNext();
+    },
+    startAnimation(duration) {
+      if (this.animation) this.animation.kill();
+      this.amount = 0;
+      this.animation = gsap.fromTo(
+        this.$data,
+        { amount: 0 },
+        {
+          ease: "none",
+          duration: duration,
+          amount: 100,
+        }
+      );
+      this.animation.then(() => {
+        this.next();
+      });
+    },
   },
-  computed: {},
+  computed: {
+    animatedAmount: function () {
+      return this.tweenedNumber.toFixed(0);
+    },
+  },
   created() {},
   mounted() {
-    setInterval(() => {
-      this.$refs.carousel.goToNext()
-    }, 1000)
+    this.$watch("$refs.carousel.currentSlide", (new_value) => {
+      this.currentSlide = new_value;
+      this.startAnimation(this.messages[new_value].duration);
+    });
+    if (this.autoStart) this.start();
   },
 };
 </script>
