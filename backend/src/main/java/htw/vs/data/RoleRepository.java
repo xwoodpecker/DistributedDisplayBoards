d2abaa2a29edb7416935cd72750307066fdb900a
Lukas Roth <lukas.roth252@gmail.com>
2021-02-16 21:07:37 +0100

    deleted backend folder from feature/frontend

--- backend/src/main/java/htw/vs/data/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package htw.vs.data;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-
-    Role findByName(String roleName);
-}
