b1a57eb22e98728d50ba3aca639bef13720659dd
Julian Altmeyer <julian.altmeyer95@gmail.com>
2021-03-01 18:33:47 +0100

    MessageRestController und Endpunkte

--- backend/src/main/java/htw/vs/data/MessageRepository.java
+++ backend/src/main/java/htw/vs/data/MessageRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Timestamp;
 import java.util.List;
 
 /**
@@ -20,4 +21,8 @@ public interface MessageRepository extends JpaRepository<Message, Long> {
      */
     @Query(value = "SELECT m from Message m WHERE m.active = true AND m.endDate < CURRENT_TIMESTAMP")
     List<Message> findMessagesToDisplay();
+    List<Message> findMessagesByBoard(Board board);
+    List<Message> findMessagesByUser(User user);
+    @Query(value = "SELECT m from Message m WHERE m.active = true AND m.endDate < CURRENT_TIMESTAMP AND m.user = :user")
+    List<Message> findActiveMessagesByUser(@Param("user")User user);
 }
---
+++ backend/src/main/java/htw/vs/rest/MessageRestController.java
@@ -0,0 +1,134 @@
+package htw.vs.rest;
+
+import htw.vs.data.*;
+import io.swagger.annotations.Api;
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@Api
+@RestController
+@RequestMapping(path = "/messages")
+public class MessageRestController {
+    private UserRepository userRepository;
+    private MessageRepository messageRepository;
+    private GroupRepository groupRepository;
+    private BoardRepository boardRepository;
+
+    public MessageRestController(UserRepository userRepository, MessageRepository messageRepository, GroupRepository groupRepository, BoardRepository boardRepository) {
+        this.userRepository = userRepository;
+        this.messageRepository = messageRepository;
+        this.groupRepository = groupRepository;
+        this.boardRepository = boardRepository;
+    }
+
+    @Operation(summary = "Get all messages")
+    @Secured("ROLE_SUPERVISOR")
+    @GetMapping(path = "/")
+    public ResponseEntity<List<Message>> getMessages(){
+        return new ResponseEntity<>(messageRepository.findAll(), HttpStatus.OK);
+    }
+
+    @Operation(summary = "Get all messages of a group")
+    @Secured({"ROLE_SUPERVISOR", "ROLE_COORDINATOR"})
+    @PreAuthorize("@securityService.hasPermission(authentication, #id)")
+    @GetMapping(path = "/group/{id}")
+    public ResponseEntity<List<Message>> getMessagesOfGroup(@PathVariable Long id){
+        ResponseEntity response;
+        Optional<Group> group = groupRepository.findById(id);
+
+        if(group.isPresent()){
+            Group temp = group.get();
+
+            Optional<Board> board = boardRepository.findById(temp.getId());
+            if(!board.isPresent()){
+                response = ResponseEntity.status(HttpStatus.NOT_FOUND).body("Board not found");
+            }
+            else {
+                response = new ResponseEntity(messageRepository.findMessagesByBoard(board.get()), HttpStatus.OK);
+            }
+        } else {
+            response = ResponseEntity.status(HttpStatus.NOT_FOUND).body("No group found");
+        }
+
+        return response;
+    }
+
+    @Operation(summary = "Get all messages of a User")
+    @GetMapping(path = "/user/{id}")
+    public ResponseEntity<List<Message>> getMessageByUser(@PathVariable Long id){
+        ResponseEntity response;
+        Optional<User> user = userRepository.findById(id);
+
+        if(user.isPresent()){
+            response = new ResponseEntity(messageRepository.findMessagesByUser(user.get()), HttpStatus.OK);
+
+        }else {
+            response = ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
+        }
+
+        return response;
+    }
+    @Operation(summary = "Get all active messages of a User")
+    @GetMapping(path = "/active/user/{id}")
+    public ResponseEntity<List<Message>> getActiveMessageByUser(@PathVariable Long id){
+        ResponseEntity response;
+        Optional<User> user = userRepository.findById(id);
+
+        if(user.isPresent()){
+            response = new ResponseEntity(messageRepository.findActiveMessagesByUser(user.get()), HttpStatus.OK);
+
+        }else {
+            response = ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
+        }
+
+        return response;
+    }
+
+    @Operation(summary = "create a new Message")
+    @Secured("ROLE_SUPERVISOR")
+    @PostMapping("/")
+    public ResponseEntity addMessage(@RequestParam Boolean active, @RequestParam Long boardId, @RequestParam Long userId, @RequestParam String content, @RequestParam Integer displayTime){
+        ResponseEntity response;
+        Message m;
+
+        Optional<Board> board = boardRepository.findById(boardId);
+        Optional<User> user = userRepository.findById(userId);
+        if(!board.isPresent()){
+            response = ResponseEntity.status(HttpStatus.NOT_FOUND).body("Board not found");
+        }
+        else if(!user.isPresent())
+        {
+            response = ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
+        }
+        else {
+            Message message = new Message();
+            message.setActive(active);
+            message.setBoard(board.get());
+            message.setUser(user.get());
+            message.setContent(content);
+            message.setDisplayTime(displayTime);
+            m = messageRepository.save(message);
+            response = new ResponseEntity(m, HttpStatus.OK);
+        }
+
+        return response;
+    }
+
+
+    @Operation(summary = "Delete a Message")
+    @Secured("ROLE_SUPERVISOR")
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteMessage(@PathVariable Long id) {
+
+        groupRepository.deleteById(id);
+        return new ResponseEntity<>(null, HttpStatus.OK);
+    }
+
+}
