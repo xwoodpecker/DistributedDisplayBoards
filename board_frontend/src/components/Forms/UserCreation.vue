904021c0d165e375962b4d5788b1a9bf92c6f6a9
Lukas Roth <lukas.roth252@gmail.com>
2021-03-21 17:22:25 +0100

    minor changes at usercreation

--- board_frontend/src/components/Forms/UserCreation.vue
+++ board_frontend/src/components/Forms/UserCreation.vue
@@ -5,50 +5,61 @@
         <md-card-header>
           <div class="md-title">Benutzer</div>
         </md-card-header>
-
         <md-card-content>
           <div class="md-layout md-gutter">
             <div class="md-layout-item md-small-size-100">
               <md-field :class="getValidationClass('username')">
                 <label>Nutzername</label>
-                <md-input name="username" id="username" autocomplete="given-name" v-model="form.username" :disabled="sending" />
-                <span class="md-error" v-if="!$v.form.username.required">The first name is required</span>
+                <md-input name="username" id="username" autocomplete="given-name" v-model="form.username"
+                          :disabled="sending"/>
+                <span class="md-error" v-if="!$v.form.username.required">Nutzername ist ein Pflichtfeld</span>
               </md-field>
             </div>
           </div>
 
           <md-field :class="getValidationClass('email')">
             <label>Email</label>
-            <md-input type="email" name="email" id="email" autocomplete="email" v-model="form.email" :disabled="sending" />
-            <span class="md-error" v-if="!$v.form.email.required">The email is required</span>
+            <md-input type="email" name="email" id="email" autocomplete="email" v-model="form.email"
+                      :disabled="sending"/>
+            <span class="md-error" v-if="!$v.form.email.required">Email muss eine valide Email-Adresse sein.</span>
           </md-field>
 
-          <md-field :class="getValidationClass('password')">
-            <label>Passwort</label>
-            <md-input type="password" name="password" id="password" autocomplete="password" v-model="form.password" :disabled="sending" />
-            <span class="md-error" v-if="!$v.form.password.required">Password is required</span>
-          </md-field>
-
-          <md-field :class="getValidationClass('isSupervisor')">
+          <md-field :class="getValidationClass('isSupervisor')" v-if="$store.getters.isSupervisor">
             <label>Supervisor</label>
-            <md-input type="checkbox" name="isSupervisor" id="isSupervisor" autocomplete="isSupervisor" v-model="form.isSupervisor" :disabled="sending" />
-            <span class="md-error" v-if="!$v.form.isSupervisor.required">Role is required</span>
+            <md-input type="checkbox" name="isSupervisor" id="isSupervisor" autocomplete="isSupervisor"
+                      v-model="form.isSupervisor" :disabled="sending"/>
+          </md-field>
+
+          <md-field v-if="user">
+            <label>
+              Passwort ändern
+            </label>
+            <md-input type="checkbox" name="changePw" id="changePw" v-model="changePw"/>
+          </md-field>
+
+          <md-field :class="getValidationClass('password')" v-show="(user && changePw) || !user">
+            <label>Passwort</label>
+            <md-input type="password" name="password" id="password" autocomplete="password" v-model="form.password"
+                      :disabled="sending"/>
           </md-field>
         </md-card-content>
 
-        <md-progress-bar md-mode="indeterminate" v-if="sending" />
+        <md-progress-bar md-mode="indeterminate" v-if="sending"/>
 
         <div class="editUser" v-if="user">
           <md-card-actions>
             <md-button class="md-warning" @click="user = null" :disabled="sending">abbrechen</md-button>
           </md-card-actions>
           <md-card-actions>
-            <md-button type="submit" class="md-primary" @click="action = 'edit'" :disabled="sending">Nutzerdaten aktualisieren</md-button>
+            <md-button type="submit" class="md-primary" @click="setAction('edit')" :disabled="sending">Nutzerdaten
+              aktualisieren
+            </md-button>
           </md-card-actions>
         </div>
 
         <md-card-actions v-if="!user && $store.getters.isSupervisor">
-          <md-button type="submit" class="md-primary" @click="action = 'create'" :disabled="sending">Nutzer erstellen</md-button>
+          <md-button type="submit" class="md-primary" @click="setAction('create')" :disabled="sending || form.password.length < 8">Nutzer erstellen
+          </md-button>
         </md-card-actions>
       </md-card>
 
@@ -58,7 +69,7 @@
 </template>
 
 <script>
-import { validationMixin } from 'vuelidate'
+import {validationMixin} from 'vuelidate'
 import {
   required,
   email,
@@ -75,12 +86,25 @@ export default {
     userSaved: false,
     sending: false,
     lastUser: null,
-    action: 'create'
+    action: 'create',
+    changePw: false,
+    form: {
+      'username': "",
+      'email': "",
+      'isSupervisor': "",
+      'password': "",
+    }
   }),
   watch: {
-    user : (user) => {
-      console.log(user)
-      //this.action = 'edit'
+    user: {
+      handler(user) {
+        this.form = {
+          'username': user ? user.userName : null,
+          'email': user ? user.email : null,
+          'isSupervisor': user ? this.isSupervisor() : false,
+          'password': "",
+        }
+      }
     }
   },
   validations: {
@@ -92,34 +116,22 @@ export default {
         required,
         email
       },
-      password: {
-        required,
-      },
-      isSupervisor: {
-        required,
-      },
     }
   },
   computed: {
-    form(){
-      return  {
-            username: this.$props.user ? this.$props.user.userName : null,
-            email: this.$props.user ? this.$props.user.email : null,
-            isSupervisor: this.$props.user ? this.isSupervisor() : false,
-            password: this.$props.user ? this.$props.user.password : null,
-      }
-    }
   },
   methods: {
-    isSupervisor()
-    {
+    isSupervisor() {
       let isSupervisor = false;
-      this.$props.user.roles.forEach( role => {
+      this.$props.user.roles.forEach(role => {
         if (role.name === "SUPERVISOR") isSupervisor = true;
       })
       return isSupervisor;
     },
-    getValidationClass (fieldName) {
+    setAction(action) {
+      this.action = action
+    },
+    getValidationClass(fieldName) {
       const field = this.$v.form[fieldName]
 
       if (field) {
@@ -128,59 +140,66 @@ export default {
         }
       }
     },
-    clearForm () {
+    clearForm() {
       this.$v.$reset()
       this.form.username = null
       this.form.isSupervisor = null
-      this.form.password = null
+      this.form.password = ""
       this.form.email = null
     },
-    saveUser () {
+    saveUser() {
       this.sending = true
       //create user
       const userToCreate = {
-        'userName' : this.form.username,
-        'password' : this.form.password,
-        'email'    : this.form.email,
-        'isSupervisor' : this.form.isSupervisor
+        'userName': this.form.username,
+        'password': this.form.password,
+        'email': this.form.email,
+        'isSupervisor': this.form.isSupervisor
       }
-      if (this.action === 'create'){
-        userapi.addUser(userToCreate).catch( error => {
+      let userToUpdate = {
+        'userName': this.form.username,
+        'email': this.form.email,
+        'isSupervisor': this.form.isSupervisor
+      }
+      if (this.changePw) {
+        userToUpdate.password = this.form.password;
+      }
+      if (this.action === 'create') {
+        userapi.addUser(userToCreate).catch(error => {
           this.$toastr.error("Fehler beim Anlegen des Benutzers");
           this.$toastr.success("Fehler beim Anlegen des Benutzers");
           this.sending = false;
           this.clearForm();
-        }).then( res => {
+        }).then(res => {
           if (res) {
-            this.$emit('user-created',userToCreate)
+            this.$emit('user-created', userToCreate)
             this.sending = false
             this.clearForm()
           }
         })
       }//update user
       else {
-        userapi.updateUser(userToCreate, this.$props.user.id).catch( error => {
+        userapi.updateUser(userToUpdate, this.$props.user.id).catch(error => {
           this.$toastr.error("Fehler beim Aktualisieren des Benutzers");
           this.sending = false;
           this.clearForm();
-        }).then( res => {
+        }).then(res => {
           if (res) {
-            this.$emit('user-updated',userToCreate)
+            this.$emit('user-updated', userToCreate)
             this.sending = false
-            this.clearForm()
           }
         })
       }
     },
-    validateUser () {
-      console.log(this.form)
-      this.saveUser()
-
+    validateUser() {
+      console.log(this.$v.form)
       //TODO Formvalidierung checken
-      /*this.$v.$touch()
+      if (this.changePw) {
+      }
+      this.$v.$touch();
       if (!this.$v.$invalid) {
         this.saveUser()
-      }*/
+      }
     }
   }
 }
